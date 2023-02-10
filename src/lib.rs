35ae42a8bf9205f490ca3e5fd367779353a0fdbb
Omnath Mandal <omnathmandal2003@gmail.com>
2023-02-10 15:07:58 +0530

    added a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
 mod cli;
 mod crud;
 pub mod interface;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn internals() {
+        use crate::crud::Crud;
+        use dotenv::dotenv;
+        use rusqlite::Connection;
+        use std::{env, path::Path};
+
+        dotenv().ok();
+        let url = env::var("DATABASE_URL").unwrap();
+        let connection = Connection::open(url).unwrap();
+
+        let mut comp = Vec::new();
+        let mut notdone = Vec::new();
+
+        let mut todo =
+            Crud { conn: connection, com: &mut comp, inc: &mut notdone };
+
+        todo.new().unwrap();
+
+        let path = Path::new("todo.db");
+        assert!(path.exists());
+    }
+}
